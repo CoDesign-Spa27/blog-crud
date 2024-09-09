a69e0e7f6dbd446a8b02fb3ab1f1eeeb71c8144b
2sandeep7 <ssandeep8696@gmail.com>
2024-09-09 17:40:07 +0530

    fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,26 @@
 const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv')
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: './database.sqlite', 
+// });
+ 
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite', 
+dotenv.config()
+// using postgres because sqllite cannot be deployed
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,  
+  dialectOptions: {
+    ssl: {
+      require: true,  
+      rejectUnauthorized: false, 
+    },
+  },
 });
+
+ 
+ 
 
 module.exports = sequelize;
